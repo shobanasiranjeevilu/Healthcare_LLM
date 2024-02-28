486245144d14515df2adf935258a830ea3bac647
shobana siranjeevilu <shobanasiranjeevi@gmail.com>
2024-02-28 15:17:00 -0600

    fixed web_Scrapping code, added visualizations, normalized dataset, summarized EDA

--- data_analysis/web_scrapping.py
+++ data_analysis/web_scrapping.py
@@ -24,8 +24,41 @@ def search_wikipedia_for_url(search_term):
         search_results = data.get("query", {}).get("search", [])
         if search_results:
             return search_results
-        
+
+
+def clean_symptoms_text(symptoms_text):
+    # Remove HTML tags
+    cleaned_text = re.sub(r'<[^>]+>', '', symptoms_text)
+    cleaned_text = re.sub(r'\[.*?\]', '', cleaned_text)
+    cleaned_text = ' '.join(cleaned_text.split())
+    return cleaned_text
+
+
+def split_symptoms(cleaned_text):
+    symptoms_list = cleaned_text.split(', ')
+    return symptoms_list 
     
+def extract_signs_and_symptoms(signs_and_symptoms_heading):
+    if signs_and_symptoms_heading:
+        content = []
+        parent_h2 = signs_and_symptoms_heading.find_parent('h2')
+        if parent_h2 is None:
+            return ''
+        for elem in parent_h2.find_next_siblings():
+            # Stop if we hit another <h2> tag (the next section)
+            if elem.name == "h2":
+                break
+            if elem.name == "p":
+                # Collect the text of the first <p> tag
+                content.append(elem.get_text(separator=" ", strip=True))
+                break
+
+        
+        symptoms_text = ' '.join(content)
+        symptoms = split_symptoms(clean_symptoms_text(symptoms_text))
+        
+        return symptoms
+
 
 
 def web_scrape_wikipedia(disease_list):
@@ -40,25 +73,34 @@ def web_scrape_wikipedia(disease_list):
         
         if url_link:
             for url in url_link:
-
+                
                 page_title = url['title']
-
+                
                 page_url = f"https://en.wikipedia.org/wiki/{page_title.replace(' ', '_')}"
                 
                 wiki = requests.get(page_url, verify=False)
                 soup = BeautifulSoup(wiki.content, 'html5lib')
-                info_table = soup.find("table", {"class": "infobox"})
+                signs_and_symptoms_heading = soup.find(id="Signs_and_symptoms")
+                
+                info_table = soup.find("table", {"class": "infobox"})         
+ 
                 if info_table:
                     for row in info_table.find_all("tr"):
                         header = row.find("th", {"scope": "row"})
                         if header and header.get_text() == "Symptoms":
                             symptom = row.find("td")
                             if symptom:
-                                
                                 symptom = ' '.join(re.sub(r'<.*?>|\[.*?\]', ' ', str(symptom)).split())
                                 diseases_with_symptoms[disease] = []
                                 diseases_with_symptoms[disease].append(page_title)
                                 diseases_with_symptoms[disease].append(symptom)
+                            
+                if signs_and_symptoms_heading is not None and disease not in diseases_with_symptoms.keys():
+                    
+                    symptom = extract_signs_and_symptoms(signs_and_symptoms_heading)
+                    diseases_with_symptoms[disease] = []
+                    diseases_with_symptoms[disease].append(page_title)
+                    diseases_with_symptoms[disease].append(symptom)
 
                 if disease in diseases_with_symptoms.keys():
                     break
